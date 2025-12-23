35b738cb02aeaf23aed78efd583d06881af84272
Jason Wolfe <jason06051998@gmail.com>
2025-12-22 21:21:08 -0600

    feat(string): added first implementation of my_strcpy()

--- my_libc.c
+++ my_libc.c
@@ -7,3 +7,11 @@ unsigned int my_strlen(const char *s){
     }
     return len;
 }
+
+//Copy the bytes from src into dest
+//User responsible for ensuring dest is large enough to hold the bytes from src
+char *my_strcpy(char *restrict dest, char *restrict src){
+    while(*src) *dest++ = *src++;
+    *dest = '\0';
+    return dest;
+}
--- my_libc.h
+++ my_libc.h
@@ -1,1 +1,2 @@
 unsigned int my_strlen(const char *s);
+char *my_strcpy(char *restrict dest, char *restrict src)

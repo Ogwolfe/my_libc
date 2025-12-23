d8f1a69f481753fc7831615a741d8496913347e6
Jason Wolfe <jason06051998@gmail.com>
2025-12-23 07:06:05 -0600

    refactor(string): changed third parameter for my_strncmp() to be size_t.

--- my_libc.c
+++ my_libc.c
@@ -1,3 +1,5 @@
+#include "my_libc.h"
+
 //Return the length of a c string
 unsigned int my_strlen(const char *s){
     unsigned len = 0;
@@ -18,4 +20,13 @@ char *my_stpcpy(char *restrict dest, const char *restrict src){
 char *my_strcpy(char *d, const char *s){
     my_stpcpy(d, s);
     return d;
+}
+
+int my_strncmp(const char *str1, const char *str2, size_t len){
+    int diff = 0;
+    while(*str1 != '\0' && len-- > 0){
+        if((diff = (*str1++ - *str2++)) != 0) return diff;
+    }
+
+    return diff;
 }
--- my_libc.h
+++ my_libc.h
@@ -1,3 +1,11 @@
+#ifndef MY_LIBC_H
+#define MY_LIBC_H
+
+#include <stddef.h>
+
 unsigned int my_strlen(const char *s);
 char *my_strcpy(char *restrict dest, const char *restrict src);
 char *my_strcpy(char *d, const char *s);
+int my_strncmp(const char *str1, const char *str2, size_t len);
+
+#endif
